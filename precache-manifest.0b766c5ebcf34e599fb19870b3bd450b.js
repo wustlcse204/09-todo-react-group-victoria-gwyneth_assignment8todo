9a827d0830546413ea92b9353a471d0b22679183
victoriacheungwustl <71216093+victoriacheungwustl@users.noreply.github.com>
2021-11-24 22:18:58 -0600

    Updates

---
+++ precache-manifest.0b766c5ebcf34e599fb19870b3bd450b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "953d00a0f58670fdf02d",
+    "url": "/09-todo-react-victoriacheungwustl/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "953d00a0f58670fdf02d",
+    "url": "/09-todo-react-victoriacheungwustl/static/js/main.953d00a0.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-victoriacheungwustl/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "618577259da755ffaa43",
+    "url": "/09-todo-react-victoriacheungwustl/static/js/runtime~main.61857725.js"
+  },
+  {
+    "revision": "9549b0884f71d0e8a36ee2d1ba103762",
+    "url": "/09-todo-react-victoriacheungwustl/index.html"
+  }
+];
