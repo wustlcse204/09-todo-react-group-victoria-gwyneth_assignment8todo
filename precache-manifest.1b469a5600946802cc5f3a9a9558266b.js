3237b1c6228d83f9c9effc06287f161fdef8111b
gwynethnoel <g.l.noel@wustl.edu>
2021-12-01 12:08:43 -0600

    Updates

--- precache-manifest.1b469a5600946802cc5f3a9a9558266b.js
+++ precache-manifest.1b469a5600946802cc5f3a9a9558266b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "55c33121b7de65f558de",
-    "url": "/09-todo-react-group-victoria-gwyneth_assignment8todo/static/css/main.4614e532.chunk.css"
+    "revision": "ee3661e535394df48318",
+    "url": "/09-todo-react-group-victoria-gwyneth_assignment8todo/static/css/main.33964020.chunk.css"
   },
   {
-    "revision": "55c33121b7de65f558de",
-    "url": "/09-todo-react-group-victoria-gwyneth_assignment8todo/static/js/main.55c33121.chunk.js"
+    "revision": "ee3661e535394df48318",
+    "url": "/09-todo-react-group-victoria-gwyneth_assignment8todo/static/js/main.ee3661e5.chunk.js"
   },
   {
     "revision": "3277a95b3abb8dec1f17",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/09-todo-react-group-victoria-gwyneth_assignment8todo/static/js/runtime~main.d6ae4d96.js"
   },
   {
-    "revision": "11303b39e9fa5e80d06d085ecfef4108",
+    "revision": "29d643b591e3873a2b156b40b37146bf",
     "url": "/09-todo-react-group-victoria-gwyneth_assignment8todo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/09-todo-react-group-victoria-gwyneth_assignment8todo/precache-manifest.8cd46bdadd0c6809aa4c19913cb9b3cb.js"
+  "/09-todo-react-group-victoria-gwyneth_assignment8todo/precache-manifest.1b469a5600946802cc5f3a9a9558266b.js"
 );
 
 workbox.clientsClaim();
